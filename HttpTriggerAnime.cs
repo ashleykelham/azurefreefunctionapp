9aff842f3bf80eda570e74c76b8d31b535def7b2
ashley kelham <neonyourself@hotmail.com>
2020-05-13 16:13:43 -0500

    loaded from cosmosdb

--- HttpTriggerAnime.cs
+++ HttpTriggerAnime.cs
@@ -1,69 +1,47 @@
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Linq;
 using azurefreefunctionapp;
+using Microsoft.Azure.Documents.Client;
 
 namespace ashleykelham
 {
     public static class HttpTriggerAnime
     {
+        const string databaseName = "anime";
+        const string collectionName = "anime";
         [FunctionName("HttpTriggerAnime")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+              [CosmosDB(
+                databaseName: databaseName,
+                collectionName: collectionName,
+                ConnectionStringSetting = "CosmosDBConnection")] IDocumentClient client,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            List<Anime> output = new List<Anime>();
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri(databaseName, collectionName);
 
-            //string name = req.Query["name"];
+          //  log.LogInformation($"Searching for: {searchterm}");
 
-           List<Anime> output = new List<Anime>(){
-               new Anime(){
-                   Name = "Attack on Titan",
-                   Id = "a1",
-                   ImageUrl = "https://www.skinit.com/media/catalog/product/cache/9dbe6a0c16a5b581719a1aa389879cfc/f/n/fnaktn03_206.jpg",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               },
-               new Anime(){
-                   Name = "Demon Slayer",
-                   Id = "a2",
-                   ImageUrl = "https://3.bp.blogspot.com/-TBIMU-SJfhg/XTOD4BzCvJI/AAAAAAAAIoA/vIyb457CZgsmvI2eku1SztpZxA1RS9gMwCKgBGAs/w600-h600-p-k-no-nu/tanjiro-kamado-nezuko-kamado-kimetsu-no-yaiba-uhdpaper.com-4K-151-wp.thumbnail.jpg",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               },
-               new Anime(){
-                   Name = "Sword Art Online",
-                   Id = "a3",
-                   ImageUrl = "https://i.pinimg.com/originals/ea/76/aa/ea76aa8a1c6f7c4f3cda4b03261c9651.jpg",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               },
-               new Anime(){
-                   Name = "Record of Grancrest War",
-                   Id = "a4",
-                   ImageUrl = "https://d2jcw5q7j4vmo4.cloudfront.net/jfwdfg3wtyyIiXXojmtLhQ2KCtKYtXjQ5TLQV8CmMtWgDk1oGLw2wmIYZJwTqLSAlg=w512",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               },
-               new Anime(){
-                   Name = "Guilty Crown",
-                   Id = "a5",
-                   ImageUrl = "https://i1.sndcdn.com/artworks-000108924962-gf5oe6-t500x500.jpg",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               },
-               new Anime(){
-                   Name = "Naruto",
-                   Id = "a6",
-                   ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTgVjuW44QxSQbdj4FHA_RV98vWlF-16519Y2MLYEpkoIunLUmn&usqp=CAU",
-                   Link = "",
-                   Description = "A young person with extraordinary potential must gain strength to save the world." 
-               }
-           };
+            IDocumentQuery<Anime> query = client.CreateDocumentQuery<Anime>(collectionUri)
+                .AsDocumentQuery();
+
+            while (query.HasMoreResults)
+            {
+                foreach (Anime result in await query.ExecuteNextAsync())
+                {
+                    output.Add(result);
+                }
+            }
 
             return new OkObjectResult(output);
         }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Azure free full stack app demo: API
 This is a .net core Azure function built as part of the [Medium article]().
 
+This API returns a collection of basic information about Anime.
+
 ### Requirements
 * Dotnet core 3.1
 * Azure Subscription
@@ -16,4 +18,4 @@ This will deploy then be able to all the function at GET http://localhost:7071/a
 * Go to the *Azure* tab in VSCode.
 * In the *FUNCTIONS* section select 'Deploy to function app'
 * Follow the onscreen instructions to select the subscription and function app to deploy it to.
-* Once complete you will be able to call your function dependant on your apps name e.g. GET https://*yourapp*.azurewebsites.net/api/HttpTriggerAnime
+* Once complete you will be able to call your function dependant on your apps name e.g. GET https://*yourapp*.azurewebsites.net/api/httptriggeranime
